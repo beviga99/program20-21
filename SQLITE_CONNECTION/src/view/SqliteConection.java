b6b7c038f9f16b696813db335e7c3cc3695b06e1
GiantsV3 <GiantsV3@192.168.0.10>
2021-03-10 20:19:33 +0100

    Ondo

---
+++ SQLITE_CONNECTION/src/view/SqliteConection.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import static model.SQLiteKudeatu.Connect;
+
+/**
+ *
+ * @author GiantsV3
+ */
+public class SqliteConection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Connect();
+    }
+    
+}
